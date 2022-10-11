78496ebf9fcabb6878fc1b6fbb73a699f3ce1e97
AlveDoesProgramming <114334244+AlveDoesProgramming@users.noreply.github.com>
2022-10-11 11:11:06 -0700

    Update oddOrEvenNumbers.py

--- oddOrEvenNumbers.py
+++ oddOrEvenNumbers.py
@@ -1,21 +1,35 @@
+from timeit import default_timer as timer
+
+
 print("This program is a game where you will only count odd numbers or even numbers starting with 1 or 2 depending on what you choose.")
 print("")
 round = 0
+highscore = 0
 name = input("Type your name/username/nickname/something: ")
 while round == 0:
+    times = 0
     quitOrAgain = 0
+    average = 0
     even = 0
     odd = -1
     x = 0
     y = -1
     oddOrEven = input("Choose between odd or even numbers. (odd/even): ")
     if oddOrEven == "even":
+
         while x == even:
+            start = timer()
             even = int(input(name+": "))
+            end = timer()
+            time = end - start
+            print(time, "sec")
+            average += time 
+            times += 1
             x += 2
         if x != even:
             print("")
             print("Wrong number. The next number was:", x)
+            print("Your average speed between numbers are:", average/times, "sec")
             quitOrAgain = int(input("Enter 1 to play again, enter 2 to quit: "))
             if quitOrAgain == 1:
                 print("You have choosen to play again")
@@ -23,23 +37,35 @@ while round == 0:
                 print("You have choosen to quit")
                 round = 1
             else:
-                print("I don't understand what you are saying so I will just assume you said you wanted to play again")    
+                print("I don't understand what you are saying so I will just assume you said you wanted to play again")  
+
     elif oddOrEven == "odd":
+
         while y == odd:
+            start = timer()
             odd = int(input(name+": "))
             y += 2
+            end = timer()
+            time = end - start
+            print(time, "sec")
+            average += time 
+            times += 1
         if y != odd:
             print("")
             print("Wrong number. The next number was:", y)
+            print("Your average speed between numbers are:", average/times, "sec")
             quitOrAgain = input("Enter 1 to play again, enter 2 to quit: ")
             if quitOrAgain == "1":
-                print("You have choosen to play again")
+                print("You have choosen to play again") 
             elif quitOrAgain == "2":
                 print("You have choosen to quit")
                 round = 1
             else:
                 print("I don't understand what you are saying so I will just assume you said you wanted to play again")
+
     else:
         print("You typed something wrong, try again")
     
+
+
 End = input("Press enter to stop the program: ")
